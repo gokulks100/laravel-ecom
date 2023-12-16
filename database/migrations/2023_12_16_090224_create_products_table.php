7fc1d472a2d00173ec089ac50a5d67dd69957de5
gokul <websoulgokulks@gmail.com>
2023-12-16 14:40:13 +0530

    first commit

---
+++ database/migrations/2023_12_16_090224_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image');
+            $table->bigIncrements('image_size');
+            $table->longText('description');
+            $table->string('status');
+            $table->decimal('price',14,2);
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
